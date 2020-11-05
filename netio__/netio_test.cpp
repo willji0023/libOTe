646db77c7a9bff0cd7011fc86a60b179769bf062
William Jin <wjin0702@yahoo.com>
2020-11-05 04:13:09 -0800

    Finish integration of NetIO

--- netio__/netio_test.cpp
+++ netio__/netio_test.cpp
@@ -1,4 +1,3 @@
-#include <emp-tool/emp-tool.h>
 #include <boost/program_options.hpp>
 #include <cryptoTools/Network/Channel.h>
 #include <cryptoTools/Network/Session.h>
@@ -7,12 +6,15 @@
 #include <cryptoTools/Common/Timer.h>
 #include <cryptoTools/Crypto/PRNG.h>
 
+#include <cryptoTools/Network/NetIO.h>
+
 using namespace osuCrypto;
-using namespace std;
 using namespace emp;
+using namespace std;
 namespace po = boost::program_options;
 
-#define LIBOTE
+// #define LIBOTE
+// #define EMPTOOLS
 
 #ifdef LIBOTE
 void __Server(int party, int port, string server_ip, uint64_t num_thrds, uint64_t num_elems){
@@ -75,6 +77,7 @@ void __Client(int party, int port, string server_ip, uint64_t num_thrds, uint64_
 	ios.stop();
 }
 #else
+#ifdef EMPTOOLS
 void __Server(int party, int port, string server_ip, uint64_t num_thrds, uint64_t num_elems){
 	osuCrypto::Timer time;
 
@@ -128,6 +131,74 @@ void __Client(int party, int port, string server_ip, uint64_t num_thrds, uint64_
 
 	io->flush();
 }
+#else
+void __Server(int party, int port, string server_ip, uint64_t num_thrds, uint64_t num_elems){
+	osuCrypto::Timer time;
+
+	IOService ios;
+	NetIO io{party==ALICE ? nullptr:server_ip.c_str(), port, true};
+	io.set_nodelay();
+
+	Channel aChl{ios, new SocketAdapter<NetIO>(io)};
+	aChl.waitForConnection();
+
+    vector<int64_t> data(num_elems);
+	PRNG prng(_mm_set_epi32(4253465, 3434565, 234435, 23987045));
+	prng.get(data.data(), data.size());
+    
+	auto s = time.setTimePoint("start");
+	aChl.send(data);
+	auto e = time.setTimePoint("finish");
+	auto milli = chrono::duration_cast<chrono::milliseconds>(e - s).count();
+	cout << "Consec: " << milli << " ms" << endl;
+    
+	io.flush();
+
+	s = time.setTimePoint("start");
+	for (uint64_t e = 0; e < num_elems; ++e)
+		aChl.send(&data[e], 1);
+	e = time.setTimePoint("finish");
+	milli = chrono::duration_cast<chrono::milliseconds>(e - s).count();
+	cout << "Seperate: " << milli << " ms" << endl;
+
+	io.flush();
+	aChl.close();
+	ios.stop();
+}
+
+void __Client(int party, int port, string server_ip, uint64_t num_thrds, uint64_t num_elems){
+	osuCrypto::Timer time;
+
+	IOService ios;
+	NetIO io{party==ALICE ? nullptr:server_ip.c_str(), port, true};
+	io.set_nodelay();
+
+	Channel aChl(ios, new SocketAdapter<NetIO>(io));
+	aChl.waitForConnection();
+
+    vector<int64_t> data(num_elems);
+
+	auto s = time.setTimePoint("start");
+	aChl.recv(data);
+	auto e = time.setTimePoint("finish");
+	auto milli = chrono::duration_cast<chrono::milliseconds>(e - s).count();
+	cout << "Consec: " << milli << " ms" << endl;
+    
+	io.flush();
+
+	s = time.setTimePoint("start");
+	for (uint64_t e = 0; e < num_elems; ++e)
+		aChl.recv(&data[e], 1);
+	e = time.setTimePoint("finish");
+	milli = chrono::duration_cast<chrono::milliseconds>(e - s).count();
+	cout << "Seperate: " << milli << " ms" << endl;
+
+	io.flush();
+	for (uint64_t t = 0; t < num_thrds; ++t)
+		aChl.close();
+	ios.stop();
+}
+#endif
 #endif
 
 int main(int argc, char** argv) {
